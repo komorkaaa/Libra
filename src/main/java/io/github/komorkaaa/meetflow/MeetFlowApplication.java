28d7823e71b64211b2d1af7d38694987161de856
komorkaaa <mr.komorka@gmail.com>
2025-12-11 22:48:29 +0300

    Initial commit

---
+++ src/main/java/io/github/komorkaaa/meetflow/MeetFlowApplication.java
@@ -0,0 +1,13 @@
+package io.github.komorkaaa.meetflow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeetFlowApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(MeetFlowApplication.class, args);
+  }
+
+}
